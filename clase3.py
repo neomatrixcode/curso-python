0726ef3b3b1720c4f4abb0dcb23c6e568ef2cdb8
neomatrix <josuecevedo@gmail.com>
2014-12-25 21:22:28 -0600

    curso python
    aprende python

---
+++ clase3.py
@@ -0,0 +1,13 @@
+
+def nombredelafuncion ():
+
+	print ("esta esta dentro de una funcion")
+
+
+if __name__ == "__main__":
+
+	nombredelafuncion()
+
+	print ("esta dentro del main")
+
+	
---
+++ hola.py
@@ -0,0 +1,23 @@
+
+
+a1= 1
+va= "hola mundo"  #string
+num= 2.5           #punto flotante
+com =2+3j
+
+
+#operaciones
+#suma,resta,multiplicacion y division
+
+a3= 1//3
+print (a3)
+
+
+
+tnum = num//3.4
+print (tnum)
+
+#otros operadores
+#< >
+#<= >=  == != 
+
